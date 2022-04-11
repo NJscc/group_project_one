993f23e06a210c2aa1af8706ecc7f5459ecf0016
Kristina Pang <kvypang@gmail.com>
2022-04-11 14:54:05 -0700

    starting pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+console.log("linked!")
+
+// Click event - movie submit button
+// Click event - recipe submit button 
+// click - randomize both catergory button 
+// fetch Spoonacular API & test 
+// fetch movie API & test 
