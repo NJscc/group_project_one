450174f85a93e61f785c04ae6d8f1cd2ea8d4c23
Kristina Pang <kvypang@gmail.com>
2022-04-13 09:15:03 -0700

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,29 +7,26 @@ console.log("linked!")
 
 var recipeKey = "5d9d9161103948ad9e6c3089d7430d7a";
 
-// test JSON URL for pasta recipes:
-
-// cuisine URLS for recipe dropdown categories
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=americancuisine
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=chinese
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=french
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=indian
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=italian
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=mexican
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=mediterranean
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=spanish
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=thai
-//https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=vietnamese
-
-
-// recipe search results appear on page 
+// recipe search on click
 $(".submitSearch").on("click", clickRecipes);
 
 function clickRecipes(event) {
     event.preventDefault()
-    var recipe = $("format-input");
+    var recipe = $("#format-input");
     var recipesLink = "https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query=" + recipe 
     fetch(recipesLink).then(function(response) {
         return response.json()
-    }).then(function() {
+    }).then(function(data) {
+        recipeChoices(data[0])
     })};
+
+function recipeChoices() {
+    var recipesLink = "https://api.spoonacular.com/recipes/complexSearch?apiKey=5d9d9161103948ad9e6c3089d7430d7a&query="
+    fetch(recipesLink).then(function(response) {
+        return response.json()
+    }).then(function(data){
+})
+}
+
+// recipes appear on page 
+// randomize recipes
